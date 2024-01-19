a8faab3ccc19528ce328f712fd173cedafae1ed4
voidNurilMustofa <147378894+voidNurilMustofa@users.noreply.github.com>
2024-01-19 15:00:38 +0700

    ok commit project

---
+++ script.js
@@ -0,0 +1,22 @@
+const menuIcon = document.getElementById("menu-icon");
+const menuList = document.getElementById("menu-list");
+
+window.addEventListener("scroll", () => {
+  const maxHeight = document.body.scrollHeight - window.innerHeight;
+  const topScrolled = (window.pageYOffset * 100) / maxHeight === 0;
+  const navbar = document.getElementsByTagName("nav")[0];
+  if (!topScrolled) {
+    navbar.classList.add("nav-box-shadow");
+  } else {
+    navbar.classList.remove("nav-box-shadow");
+  }
+});
+
+menuIcon.addEventListener("click", () => {
+  menuList.classList.toggle("hidden");
+});
+
+const btnContact = document.getElementsByClassName(".btn-contatct")[0];
+btnContact.addEventListener("click", ()=>{
+  
+});
